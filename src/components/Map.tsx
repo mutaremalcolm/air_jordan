34b3839a9a8e4c9fc51c1bbd11290d2761e6d348
“malcolmmutare” <mutarem@gmail.com>
2025-02-02 17:39:55 +0200

    feat: created the map component

---
+++ src/components/Map.tsx
@@ -0,0 +1,70 @@
+import React, {useRef, useEffect, useState} from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+
+const accessToken = process.env.MAPBOXGL_ACCESSTOKEN;
+
+interface MapProps {
+    accessToken: any;
+    lat: number;
+    lon: number;
+}
+
+const Map: React.FC<MapProps> = ({ lat, lon }) => {
+    const mapContainer = useRef<HTMLDivElement | null>(null);
+    const map = useRef<mapboxgl.Map | null>(null);
+    const marker = useRef<mapboxgl.Marker | null>(null);
+    const [zoom, setZoom] = useState(2)
+
+    useEffect(() => {
+        if (!map.current && mapContainer.current) {
+            const container: string | HTMLElement = mapContainer.current;
+            map.current = new mapboxgl.Map({
+                container,
+                style: 'mapbox://styles/mapbox/streets-v12',
+                center: [lon, lat],
+                zoom: zoom,
+            });
+
+            map.current.addControl(
+                new mapboxgl.GeolocateControl({
+                    positionOptions: {
+                        enableHighAccuracy: true,
+                    },
+                    trackUserLocation: false,
+                    showUserHeading: true,
+                })
+            );
+
+            //Create a marker and add it to the map
+            marker.current = new mapboxgl.Marker()
+                .setLngLat([lon, lat])
+                .addTo(map.current);
+                //Repeating this will add more markers to the map
+                //Need to map through an array of locations to render a cluster
+                //From there, add popups with AQI data from API
+
+        } else if (map.current && marker.current) {
+            // Check if user's input coordinates have changed
+            const currentCenter = map.current.getCenter();
+            if (lat !== currentCenter.lat || lon !== currentCenter.lng) {
+                //Use the `flyTo` method to smoothly transition to the new coordinates
+                map.current.flyTo({
+                    center: [lon, lat],
+                    zoom: 17, //Desired zoom level
+                    essential: true, //This E$nsures the animation is not cancelled by user interactions
+                    duration: 15000
+                });
+
+                //Update the marker's position
+                marker.current.setLngLat([lon, lat]);
+            }
+        }
+    }, [lat, lon, zoom])
+
+    return (
+        <div>
+            <div ref={mapContainer} id='map-container'></div>
+        </div>
+    )
+}
