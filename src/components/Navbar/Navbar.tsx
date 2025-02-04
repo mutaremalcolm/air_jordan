3f071dd53881535893f54818f702e05638e90ce9
“malcolmmutare” <mutarem@gmail.com>
2025-02-04 11:39:48 +0200

    feat: added about modal to offer more description about the project for users who are curious

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Menu from '../../../public/menu2.png';
 import Image from 'next/image';
 import Logo from '../../../public/airLogo.png';
+import AboutModal from './AboutModal';
 
 interface NavBarProps {
     //types
@@ -23,9 +24,12 @@ const Navbar: React.FC<NavBarProps> = () => {
                 <Image src={Logo} width={30} height={30} className='h-10 mr-3 w-10' alt='logo'/>
                 <span className='hidden md:block self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>AIR JORDAN</span>
             </a>
-        </div>
-        
-        
+            <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto 2xl:flex 2xl:p-2`} id='navbar-default' >
+                <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+                    <li><AboutModal /></li>
+                </ul>
+            </div>
+        </div>     
     </nav>
     </>
   )
