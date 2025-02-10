1baf76ec5ef6320ed8eecc2576b24513d0f8414b
“malcolmmutare” <mutarem@gmail.com>
2025-02-10 20:36:08 +0200

    fix: updated settings for font import

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,11 @@ import Navbar from './Navbar/Navbar';
 import Footer from './Footer/Footer';
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ 
+            subsets: ['latin'],
+            display: 'swap',
+            adjustFontFallback: false 
+        })
 
 
 interface LayoutProps {
