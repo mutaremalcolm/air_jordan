1357430b517dee68d75a516917f9612b8cf3a612
“malcolmmutare” <mutarem@gmail.com>
2025-02-12 18:57:46 +0200

    feat: added mobile menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -25,6 +25,10 @@ const Navbar: React.FC<NavBarProps> = () => {
                 <Image src={Logo} width={30} height={30} className='h-10 mr-3 w-10' alt='logo'/>
                 <span className='hidden md:block self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>AIR JORDAN</span>
             </a>
+            <button onClick={handleMenuToggle} data-collapse-toggle='navbar-default' type='button' className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' aria-controls='navbar-default' aria-expanded={isMenuOpen}>
+                <span className='sr-only'>Open Main Menu</span>
+                <Image src={Menu} width={30} alt='Burger Menu Icon' />
+            </button>
             <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto 2xl:flex 2xl:p-2`} id='navbar-default' >
                 <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
                     <li><AboutModal /></li>
